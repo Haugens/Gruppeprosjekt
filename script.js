d318d505e148b3aed7d995e5c39e881fb9465390
HeinKaptein <tvehei16@student.westerdals.no>
2017-05-11 15:01:53 +0200

    marker icons

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         // Styles a map in night mode.
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 59.9239, lng: 10.7578},
-          zoom: 15,
+          zoom: 14,
           styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
@@ -83,6 +83,24 @@
               stylers: [{color: '#17263c'}]
             }
           ]
+        });
+          var image = 'https://cdn0.iconfinder.com/data/icons/sports-glyph-black/2048/356_-_Weight_Lifting_Person-512.png';
+        var training = new google.maps.Marker({
+          position: {lat: 59.92722418, lng: 10.74247777},
+          map: map,
+          icon: image
+        });
+          var image = 'https://cdn4.iconfinder.com/data/icons/sports-recreation/128/running-2-512.png';
+        var training = new google.maps.Marker({
+          position: {lat: 59.91852701, lng: 10.75600147},
+          map: map,
+          icon: image
+        });
+            var image = 'https://cdn3.iconfinder.com/data/icons/common-sports/4096/basketball-512.png';
+        var training = new google.maps.Marker({
+          position: {lat: 59.9229908, lng: 10.7548652},
+          map: map,
+          icon: image
         });
       }
     
