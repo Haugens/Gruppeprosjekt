ae5f878abd5f30a4268d357430f5db4a123357a7
Hekkii <hekkiiwalters@gmail.com>
2017-05-28 12:43:04 +0200

    commit ikke ødelegg
    må endre på main.css

---
+++ JS/fixedfix.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var height = $("#menybar").height();
+    if(height > 0)
+    {
+        $("#calendar_container").css("margin-top",height-10);
+    }
+});
+
+$(window).resize(function() {
+    var height = $("#menybar").height();
+    if(height > 0)
+    {
+        $("#calendar_container").css("margin-top",height-10);
+    }
+});
+
